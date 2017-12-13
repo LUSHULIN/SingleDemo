c396734609779f19ebfc4c8e0e5788a718adcb97
Jason <Jason@JasonMacPro.local>
2017-12-13 15:31:56 +0800

    submit child file

---
+++ SingleDemo/SingleDemo/Child.h
@@ -0,0 +1,13 @@
+//
+//  Child.h
+//  SingleDemo
+//
+//  Created by Jason on 2017/12/13.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Child : NSObject
+
+@end
---
+++ SingleDemo/SingleDemo/Child.m
@@ -0,0 +1,13 @@
+//
+//  Child.m
+//  SingleDemo
+//
+//  Created by Jason on 2017/12/13.
+//  Copyright © 2017年 Jason. All rights reserved.
+//
+
+#import "Child.h"
+
+@implementation Child
+
+@end
